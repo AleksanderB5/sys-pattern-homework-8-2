ab85b946821b0b72037cc9e5d00d3f9a89c97e64
Aleks <aleks@BIG.Home>
2025-07-18 18:28:45 +0300

    1

---
+++ terraform/storage.tf
@@ -0,0 +1,19 @@
+resource "yandex_storage_bucket" "bucket" {
+  bucket     = "n15-bucket"
+  access_key = yandex_iam_service_account_static_access_key.n15.access_key
+  secret_key = yandex_iam_service_account_static_access_key.n15.secret_key
+
+  anonymous_access_flags {
+    read = true
+    list = true
+  }
+}
+
+resource "yandex_storage_object" "netology-logo" {
+  bucket     = yandex_storage_bucket.bucket.bucket
+  key        = "zad.jpeg"
+  source     = "zad.jpeg"
+  access_key = yandex_iam_service_account_static_access_key.n15.access_key
+  secret_key = yandex_iam_service_account_static_access_key.n15.secret_key
+  acl        = "public-read"
+}
